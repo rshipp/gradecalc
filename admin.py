211190e148d0e9d96048da88d582f2c8e67b5a40
george <rpubaddr0@gmail.com>
2014-12-26 14:28:40 -0700

    Register models in the admin.

--- admin.py
+++ admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+import models
+
+admin.site.register(models.PointsCourse)
+admin.site.register(models.PercentCourse)
+admin.site.register(models.Group)
+admin.site.register(models.PointsAssignment)
+admin.site.register(models.PercentAssignment)

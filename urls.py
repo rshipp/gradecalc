b6f79c409b7b080b036df95da1d16a941879d5a1
george <rpubaddr0@gmail.com>
2014-12-26 15:09:06 -0700

    Added basic templates and an index url.

--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='gradecalc/index.html'),
+        name='index'),
 )
